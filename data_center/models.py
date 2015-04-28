c11db3b879dcd11cbe9d09503fca3beaddec082e
hydai <z54981220@gmail.com>
2015-04-28 22:18:00 +0800

    Add data schema

---
+++ data_center/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Course(models.Model):
+    """Course database schema"""
+    note            = models.TextField()
+    object          = models.TextField()
+    time            = models.TextField()
+    no              = models.TextField()
+    title           = models.TextField()
+    teacher         = models.TextField()
+    room            = models.TextField()
+    credit          = models.IntegerField()
+    limit           = models.IntegerField()
+    prerequisite    = models.BooleanField(default=False)
