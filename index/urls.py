6e221f2d5b2138b3e53ebe48ca3efcb4e32f61ae
hydai <z54981220@gmail.com>
2015-04-30 13:58:39 +0800

    Finish symbol link

--- index/urls.py
+++ index/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 import views
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.index),
+    url(r'^index.html$', TemplateView.as_view(template_name='index.html')),
+    url(r'^donate.html$', TemplateView.as_view(template_name='donate.html')),
+    url(r'^todo-list.html$', TemplateView.as_view(template_name='todo-list.html')),
+    url(r'^404.html$', TemplateView.as_view(template_name='404.html')),
 )
