5c743eeea3c6c2fbd280bc1b6b8b3f08ddea9dad
hydai <z54981220@gmail.com>
2015-04-30 13:42:59 +0800

    Enroll app and setup url

--- NTHU_Course/settings.py
+++ NTHU_Course/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'index',
     'data_center',
     'crawler',
 )
@@ -81,5 +82,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
