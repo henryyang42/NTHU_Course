2286bdd9cf973c7805418959f8f58bc90f9d85ff
henryyang42 <henryyang42@gmail.com>
2015-05-07 17:02:28 +0800

    change autocomplete implementation

--- search/static/js/search.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('#courseSearch').yourlabsAutocomplete({
-    url: '/search'
-  }).input.bind('selectChoice', function(e, choice, autocomplete) {
-    // When a choice is selected, open it.
-    window.location.href = choice.attr('href');
-  });
-})
