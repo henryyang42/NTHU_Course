a274588d23500561160e660c5165d04f86e0f06e
henryyang42 <henryyang42@gmail.com>
2015-05-29 00:09:46 +0800

    change semester

--- table/static/js/selectCourse.js
+++ table/static/js/selectCourse.js
@@ -1,5 +1,5 @@
 var moduleNTHUCourse = angular.module("ModuleNTHUCourse", ['ui.bootstrap']);
-var semester = '10320';
+var semester = '10410';
 
 moduleNTHUCourse.filter('courseInThatTime', function() {
   return function(input, time) {
