2890b98642f1fc94bdc1d6db8dee94a4b671731a
hydai <z54981220@gmail.com>
2015-05-28 21:58:17 +0800

    Update saveAsFile.js

--- table/static/js/saveAsFile.js
+++ table/static/js/saveAsFile.js
@@ -1,9 +1,4 @@
 $("#saveAsFile").click(function() {
-  console.log("Click " + $("#course-table").css("border-color"));
-  $("#course-table").css("border", "1px solid black")
-  $("#course-table > tr").css("border", "1px solid black");
-  $("#course-table > th").css("border", "1px solid black");
-  $("#course-table > td").css("border", "1px solid black");
   html2canvas($("#course-table")).then(function(canvas) {
     var bgcanvas = document.createElement("canvas");
     bgcanvas.width = canvas.width;
@@ -14,13 +9,7 @@ $("#saveAsFile").click(function() {
     gradient.addColorStop(1, "rgba(221, 232, 255, 0.9)");
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    console.log(canvas.width + " " + canvas.height);
-    //document.body.appendChild(canvas);
-    console.log(bgcanvas.width + " " + bgcanvas.height);
-    //document.body.appendChild(bgcanvas);
     ctx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
-    console.log(bgcanvas.width + " " + bgcanvas.height);
-    document.body.appendChild(bgcanvas);
 
     var data = bgcanvas.toDataURL("image/png");
     data = data.replace("image/png", "image/octet-stream");
@@ -32,8 +21,4 @@ $("#saveAsFile").click(function() {
     downloadLink.click();
     document.body.removeChild(downloadLink);
   });
-  $("#course-table").css("border-color", "");
-  $("#course-table > tr").css("border-color", "");
-  $("#course-table > th").css("border-color", "");
-  $("#course-table > td").css("border-color", "");
 });
