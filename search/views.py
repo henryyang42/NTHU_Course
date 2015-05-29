247e1292e4ab826de96bfc892cd6996f2e4872b7
hydai <z54981220@gmail.com>
2015-05-29 16:27:10 +0800

    100 -> 101

--- search/views.py
+++ search/views.py
@@ -26,8 +26,8 @@ def get_dept(no):
     if len(no) == len('101062142') - 1:
         no = '0' + no
     year = no[0:3]
-    if (int(year) < 100):
-        year = "100";
+    if (int(year) < 101):
+        year = "101";
     dept = no[3:6]
     dept = DEPT_MAP.get(dept, '')
     class_name = no[6:7]
