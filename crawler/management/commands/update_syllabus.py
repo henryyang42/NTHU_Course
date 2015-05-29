96e1bb2c048f92898d80f7ca037df321f8715d00
henryyang42 <henryyang42@gmail.com>
2015-05-29 16:34:15 +0800

    pass pyflakes

--- crawler/management/commands/update_syllabus.py
+++ crawler/management/commands/update_syllabus.py
@@ -1,8 +1,6 @@
 from django.core.management.base import BaseCommand
 
 from crawler.crawler import update_syllabus
-from data_center.const import cou_codes
-from data_center.models import Course, Department
 
 
 class Command(BaseCommand):
@@ -12,6 +10,3 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         if len(args) == 0:
             update_syllabus()
-
-
-
