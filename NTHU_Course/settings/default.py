72290e4c14f02ddc1c31d12e44f6089f7f6effd9
Salas <ga.e.m.project@gmail.com>
2015-06-08 02:55:18 +0000

    use django.request template_context process

--- NTHU_Course/settings/default.py
+++ NTHU_Course/settings/default.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf import global_settings
 import os
 import secret_key
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -66,8 +67,8 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+    'django.core.context_processors.request',
     'social.apps.django_app.context_processors.backends',
     'social.apps.django_app.context_processors.login_redirect',
 )
