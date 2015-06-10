a02cec6a13a43fbada99e1099ae0dcebc6828194
Salas <ga.e.m.project@gmail.com>
2015-06-10 21:25:03 +0000

    facebook user model and template

--- member/views.py
+++ member/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from member.models import Member
 
+
 def save_userdata(backend, user, response, *args, **kwargs):
     if backend.name == 'facebook':
         try:
             profile = Member.objects.get(user_id=user.id)
         except Member.DoesNotExist:
             profile = Member(user_id=user.id)
-        profile.gender = response.get('gender')
-        profile.link = response.get('link')
-        profile.timezone = response.get('timezone')
-        profile.department= '???????'
+        profile.uuid = response.get('id')
+        profile.email = response.get('email')
+        profile.access_token = response.get('access_token')
         profile.save()
