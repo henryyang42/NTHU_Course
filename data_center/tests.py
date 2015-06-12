402906cc428e490e77f6e1d8e93ff28ec6ee542e
afg <afg984@gmail.com>
2015-06-12 23:31:59 +0800

    tests

---
+++ data_center/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from data_center.modules import Course
+
+
+class ModelTest(TestCase):
+
+    def test_course_can_be_created_with_only_no(self):
+        Course.objects.create(no='12345QAQ 010101')
+
+        self.assertEqual(1, Course.objects.count())
+
+    def test_this_test_shall_fail(self):
+        self.fail()
