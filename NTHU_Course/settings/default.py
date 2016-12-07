e45ed77257f19d0330c235928c24ea2401885733
henryyang42 <henryyang42@gmail.com>
2016-12-07 23:12:35 +0800

    remove useless blank line

--- NTHU_Course/settings/default.py
+++ NTHU_Course/settings/default.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
